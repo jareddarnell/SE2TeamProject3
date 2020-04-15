12c954ef2831e847f934b9efc035c01e0a6b442e
Jared Darnell <jareddarnell@mail.weber.edu>
2020-04-14 20:03:48 -0600

    hub updates

--- Final_Project/Hubs/ChatHub.cs
+++ Final_Project/Hubs/ChatHub.cs
@@ -1,13 +1,44 @@
 ﻿using Microsoft.AspNetCore.SignalR;
 using System.Threading.Tasks;
 
-namespace SignalRChat.Hubs
+namespace GroupOrganizer.Hubs
 {
     public class ChatHub : Hub
     {
         public async Task SendMessage(string user, string message)
         {
+            string tempUser;
+            object value;
+            //check if key "userName" exists in context.items dictionary
+            if (Context.Items.TryGetValue("userName", out value))
+            {
+                //username exists, check to see the username recived matches whats in the dictionary
+                tempUser = value.ToString();
+                if (tempUser == user)
+                {
+                    user = tempUser;
+                }
+                else
+                {
+                    //username didn't match, remove and add in new username
+                    Context.Items.Remove("userName");
+                    Context.Items.Add("userName", user);
+                }
+            }
+            else
+            {
+                //no value for "userName" existed in the dictionary, add in the username that was recived
+                Context.Items.Add("userName", user);
+            }
+
+
+            //send username and message to all connected clients.
             await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        public async Task ReceiveGroup(string group)
+        {
+            await Clients.All.SendAsync("ReceiveGroup", group);
         }
     }
 }

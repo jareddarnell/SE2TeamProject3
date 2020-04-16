a8587b6a8c0d45a27caeab6665906cf9699bf615
Jared Darnell <jareddarnell@mail.weber.edu>
2020-04-15 22:42:09 -0600

    typo fix

--- Final_Project/wwwroot/js/chat.js
+++ Final_Project/wwwroot/js/chat.js
@@ -3,17 +3,24 @@
 //var connection = new signalR.HubConnectionBuilder().withUrl("http://students.cs.weber.edu/Group123/chatHub").build(); // Server setting
 var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build(); //Local Setting
 
+//connection.start();
+
+
 connection.start().then(function ()
 {
-
-}).catch(function (err) {
-    return console.error(err.toString));
+    
+} ).catch(function (err)
+{
+    return console.error(err.toString());
 } );
+
+
 
 connection.on("ReceiveMessage", function (user, message)
 {
     
 } );
+
 
 //Login Event
 document.getElementById("login").addEventListener("click", function (eventLogin)

d3b1072635df7a8feddc4b20390b169997dca9ec
Tucker Brady <tuck.al.brady@gmail.com>
2020-04-16 21:28:46 -0600

    * Group.cs: added group class
    * bootstrap.min.js 2.map:
    * jquery.validate 2.js:
    * jquery.validate.min 2.js:
    * .bootstrap.min.js 2.map.icloud:
    * additional-methods.min 2.js:
    * .jquery.validate 2.js.icloud:
    * .jquery.validate.min 2.js.icloud:
    * .additional-methods.min 2.js.icloud:
    * jquery.validate.unobtrusive 2.js:
    * jquery.validate.unobtrusive.min 2.js:
    * .jquery.validate.unobtrusive 2.js.icloud:
    * .jquery.validate.unobtrusive.min 2.js.icloud:
    
    * Items.cs: added Item class
    
    * WebObjects.cs: added web objects class

---
+++ Final_Project/Group.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Final_Project
+{
+    public class Group
+    {
+        //Global Variables
+        #region variables
+        /// <summary>
+        /// This will hold the user's name
+        /// </summary>
+        public string sGroupName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// This will hold the List of Item Objects
+        /// </summary>
+        
+
+
+        #endregion
+
+        #region methods
+        //Create a List of Items
+        public List<Group> lGroupList = new List<Group>();
+
+
+
+        #endregion
+
+
+
+
+
+    }// end class
+}// end namespace
+
